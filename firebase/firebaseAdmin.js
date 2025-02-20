f8115a39cda50c548359cecf2f807a91c89a15ea
nghia260324 <nghia260324@gmail.com>
2025-02-20 15:36:27 +0700

    Update Firebase Admin

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,6 +1,14 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./firebaseAdmin.json");
+//var serviceAccount = require("./firebaseAdmin.json");
+
+let serviceAccount;
+
+if (fs.existsSync("./firebase/firebaseAdmin.json")) {
+  serviceAccount = require("./firebaseAdmin.json");
+} else {
+  serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
