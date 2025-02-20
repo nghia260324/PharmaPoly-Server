e08b04b03da069ee5fdc2440aadd14ab474d297d
nghia260324 <nghia260324@gmail.com>
2025-02-20 15:48:08 +0700

    Update bim/www

--- config/db.js
+++ config/db.js
@@ -10,8 +10,8 @@ const local = `mongodb+srv://${username}:${password}@cluster0.y421t.mongodb.net/
 const connect = async () => {
     try {
         await mongoose.connect(local, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            //useNewUrlParser: true,
+            //useUnifiedTopology: true,
         })
         console.log('Connect succes!');
     } catch (err) {
