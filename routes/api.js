a40bca0d8fca56bcf6ee14dea55a675c3d79720b
nghia260324 <nghia260324@gmail.com>
2025-03-02 22:54:32 +0700

    Update Authen

--- routes/api.js
+++ routes/api.js
@@ -34,7 +34,7 @@ function authenticateToken(req, res, next) {
     // if (process.env.NODE_ENV === 'development') {
     //     return next();
     // }
-    return next();
+    // return next();
     const token = req.headers['authorization']?.split(' ')[1];
     if (!token) {
         return res.status(401).json({
